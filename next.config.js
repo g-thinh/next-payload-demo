b664c3b436545021db17e78a9797505bd3b50780
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-07-19 19:21:31 -0400

    install payloadcms

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require("path");
+const { withPayload } = require("@payloadcms/next-payload");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPayload(nextConfig, {
+  // Point to your Payload config (Required)
+  configPath: path.resolve(__dirname, "./src/payload/payload.config.ts"),
+
+  // Point to your exported, initialized Payload instance (optional, default shown below`)
+  payloadPath: path.resolve(process.cwd(), "./src/payload/payloadClient.ts"),
+});
