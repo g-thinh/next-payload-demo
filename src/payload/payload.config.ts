d89eaae274f5f9f7b6441766b7bc00e847139279
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-07-20 07:40:14 -0400

    add email smtp to build config

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,5 +1,5 @@
-import path from 'path';
-import { buildConfig } from 'payload/config';
+import path from "path";
+import { buildConfig } from "payload/config";
 
 export default buildConfig({
   collections: [
@@ -8,7 +8,20 @@ export default buildConfig({
   globals: [
     // Your globals here
   ],
+  email: {
+    transportOptions: {
+      host: process.env.SMTP_HOST,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+      port: process.env.SMTP_PORT,
+      secure: false,
+    },
+    fromName: process.env.SMTP_FROM_NAME ?? "",
+    fromAddress: process.env.SMTP_FROM_ADDRESS ?? "",
+  },
   typescript: {
-    outputFile: path.resolve(__dirname, '../payload-types.ts'),
+    outputFile: path.resolve(__dirname, "../payload-types.ts"),
   },
 });
